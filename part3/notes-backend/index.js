e94a521c036de98ad8db4f3d1205e9fb7c181f7a
Raimundo Pardo <raipardo.net@gmail.com>
2026-01-11 18:59:19 -0300

    part 3 b 5

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -33,6 +33,8 @@ app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
+app.use(express.static("dist"));
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
