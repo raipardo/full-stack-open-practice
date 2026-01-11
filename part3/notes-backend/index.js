7abdab193ef4795fa09800cecd56f93649c039ca
Raimundo Pardo <raipardo.net@gmail.com>
2026-01-11 18:29:47 -0300

    part3 b 1

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 let notes = [
   {
@@ -26,6 +27,8 @@ const requestLogger = (request, response, next) => {
   console.log("---");
   next();
 };
+
+app.use(cors());
 
 app.use(express.json());
 app.use(requestLogger);
@@ -88,7 +91,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
