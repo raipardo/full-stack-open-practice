47b4e2a40f4293b94f87236533ee4292ba422431
Raimundo Pardo <raipardo.net@gmail.com>
2026-01-13 12:47:31 -0300

    part4 a

--- part3/notes-backend/models/note.js
+++ part3/notes-backend/models/note.js
@@ -1,24 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose
-  .connect(url, { family: 4 })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
     required: true,
+    minlength: 5
   },
   important: Boolean,
 })
@@ -28,7 +14,7 @@ noteSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  },
+  }
 })
 
 module.exports = mongoose.model('Note', noteSchema)
