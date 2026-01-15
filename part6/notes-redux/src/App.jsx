4a734b7924d9e2dd9717fc7f14c324a55bd668ab
Raimundo Pardo <raipardo.net@gmail.com>
2026-01-15 12:16:25 -0300

    part6 c

--- part6/notes-redux/src/App.jsx
+++ part6/notes-redux/src/App.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import NoteForm from './components/NoteForm'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
 
+import { initializeNotes } from './reducers/noteReducer'
+
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeNotes())
+  }, [dispatch])
+
   return (
     <div>
       <NoteForm />
